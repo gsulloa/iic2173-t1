a88c1bbd87122b5641788d57fefce846dedd580e
Gabriel Ulloa <gsulloa@uc.cl>
2018-09-03 19:06:50 -0300

    form to post comments and obtain time and ip

--- index.py
+++ index.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, render_template
+from time import gmtime, strftime
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        return 'Index post'
-    else:
-        return render_template('comments/index.html')
+        comment = request.form['comment']
+        client_ip = request.remote_addr
+        time = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+        print(comment, client_ip, time)
+    return render_template('comments/index.html')
